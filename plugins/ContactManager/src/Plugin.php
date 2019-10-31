3cb5b3f2dd00353df651577686add3b5ef651199
Murata Miwa <murata@tara.co.jp>
2019-10-31 10:40:25 +0900

    各種ロード

--- plugins/ContactManager/src/Plugin.php
+++ plugins/ContactManager/src/Plugin.php
@@ -3,10 +3,26 @@
 namespace ContactManager;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
 
 /**
  * Plugin for ContactManager
  */
 class Plugin extends BasePlugin
 {
+    public function middleware($middleware){
+        return $middleware;
+    }
+
+    public function console($commands){
+        return $commands;
+    }
+
+    public function bootstrap(PluginApplicationInterface $app){
+        parent::bootstrap($app);
+    }
+
+    public function routes($routes){
+        parent::routes($routes);
+    }
 }
